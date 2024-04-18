32cad8d1b3494d288993b95040ffca2bd601006b
Shankar Vallabhan A <shankarmanu@protonmail.com>
2024-04-18 19:02:27 +0530

    Added Turnip Build Script
    Make sure your device android version is 13 and above , also ensure latest magisk / kernelsu is installed

---
+++ build-turnip.sh
@@ -0,0 +1,209 @@
+#!/bin/bash -e
+
+# Define required packages for building the turnip driver
+deps="meson ninja patchelf unzip curl pip flex bison zip"
+
+# Define directory variables
+workdir="$(pwd)/turnip_workdir"
+magiskdir="$workdir/turnip_module"
+
+# Define Android NDK version
+ndkver="android-ndk-r26d"
+
+# Define colors for terminal output
+green='\033[0;32m'
+red='\033[0;31m'
+nocolor='\033[0m'
+
+clear
+
+# Clean work directory if it exists
+if [ -d "$workdir" ]; then
+    echo "Work directory already exists. Cleaning before proceeding..." $'\n'
+    rm -rf "$workdir"
+fi
+
+echo "Checking system for required dependencies..."
+
+# Check for required dependencies and install missing ones if any
+for deps_chk in $deps; do
+    sleep 0.25
+    if command -v "$deps_chk" >/dev/null 2>&1; then
+        echo -e "$green - $deps_chk found $nocolor"
+    else
+        echo -e "$red - $deps_chk not found, cannot continue. $nocolor"
+        deps_missing=1
+    fi
+done
+
+# Install missing dependencies automatically
+if [ "$deps_missing" == "1" ]; then
+    echo "Missing dependencies, installing them now..." $'\n'
+    sudo apt install -y meson patchelf unzip curl python3-pip flex bison zip &> /dev/null
+fi
+
+clear
+
+# Install python Mako dependency
+echo "Installing python Mako dependency (if missing)..." $'\n'
+python3 -m pip install mako &> /dev/null
+
+echo "Creating and entering the work directory..." $'\n'
+mkdir -p "$workdir" && cd "$_"
+
+# Download Android NDK
+echo "Downloading Android NDK..." $'\n'
+curl https://dl.google.com/android/repository/"$ndkver"-linux.zip --output "$ndkver"-linux.zip &> /dev/null
+
+clear
+
+echo "Extracting Android NDK to a folder..." $'\n'
+unzip "$ndkver"-linux.zip &> /dev/null
+
+# Download Mesa source code
+echo "Downloading Latest Mesa source from the main branch..." $'\n'
+curl https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip --output mesa-main.zip &> /dev/null
+
+clear
+
+echo "Extracting Mesa source to a folder..." $'\n'
+unzip mesa-main.zip &> /dev/null
+cd mesa-main
+
+clear
+
+# Create Meson cross file for Android
+echo "Creating Meson cross file..." $'\n'
+ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
+cat <<EOF >"android-aarch64"
+[binaries]
+ar = '$ndk/llvm-ar'
+c = ['ccache', '$ndk/aarch64-linux-android33-clang', '-O2']
+cpp = ['ccache', '$ndk/aarch64-linux-android33-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++', '-O2']
+c_ld = 'lld'
+cpp_ld = 'lld'
+strip = '$ndk/aarch64-linux-android-strip'
+pkgconfig = ['env', 'PKG_CONFIG_LIBDIR=NDKDIR/pkgconfig', '/usr/bin/pkg-config']
+[host_machine]
+system = 'android'
+cpu_family = 'aarch64'
+cpu = 'armv8'
+endian = 'little'
+EOF
+
+# Generate build files using Meson
+echo "Generating build files..." $'\n'
+meson build-android-aarch64 --cross-file "$workdir"/mesa-main/android-aarch64 -Dbuildtype=release -Dplatforms=android -Dplatform-sdk-version=33 -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Db_lto=true &> "$workdir"/meson_log
+
+# Compile build files using Ninja
+echo "Compiling build files..." $'\n'
+ninja -C build-android-aarch64 &> "$workdir"/ninja_log
+
+echo "Using patchelf to match soname..." $'\n'
+cp "$workdir"/mesa-main/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so "$workdir"
+cd "$workdir"
+
+if ! [ -a libvulkan_freedreno.so ]; then
+    echo -e "$red Build failed! libvulkan_freedreno.so not found $nocolor" && exit 1
+fi
+
+echo "Prepare magisk module structure..." $'\n'
+p1="system/vendor/lib64/hw"
+mkdir -p "$magiskdir/$p1"
+cd "$magiskdir"
+
+echo "Copy necessary files from the work directory..." $'\n'
+cp "$workdir"/libvulkan_freedreno.so "$workdir"/vulkan.adreno.so
+cp "$workdir"/vulkan.adreno.so "$magiskdir/$p1"
+
+meta="META-INF/com/google/android"
+mkdir -p "$meta"
+
+# Create update-binary
+cat <<EOF >"$meta/update-binary"
+#################
+# Initialization
+#################
+umask 022
+# echo before loading util_functions
+ui_print() { echo "\$1"; }
+require_new_magisk() {
+  ui_print "*******************************"
+  ui_print " Please install Magisk v20.4+! "
+  ui_print "*******************************"
+  exit 1
+}
+#########################
+# Load util_functions.sh
+#########################
+OUTFD=\$2
+ZIPFILE=\$3
+[ -f /data/adb/magisk/util_functions.sh ] || require_new_magisk
+. /data/adb/magisk/util_functions.sh
+[ \$MAGISK_VER_CODE -lt 20400 ] && require_new_magisk
+install_module
+exit 0
+EOF
+
+# Create updater-script
+cat <<EOF >"$meta/updater-script"
+#MAGISK
+EOF
+
+# This will check for any issues and also provide correct PATH for module to install in KernelSu 
+
+cat <<EOF >"module.prop"
+id=turnip-mesa
+name=Freedreno Turnip Vulkan Driver
+version=v24.1
+versionCode=1
+author=V3KT0R-87
+description=Turnip is an open-source vulkan driver for devices with Adreno 6xx GPUs.
+EOF
+
+cat <<EOF >"customize.sh"
+MODVER=\`grep_prop version \$MODPATH/module.prop\`
+MODVERCODE=\`grep_prop versionCode \$MODPATH/module.prop\`
+
+ui_print ""
+ui_print "Version=\$MODVER Dev"
+ui_print "MagiskVersion=\$MAGISK_VER"
+ui_print ""
+ui_print "Freedreno Turnip Vulkan Driver -V3KT0R"
+ui_print "Adreno Driver Support Group - Telegram"
+ui_print ""
+sleep 1.25
+
+ui_print ""
+ui_print "Checking Device info ..."
+sleep 1.25
+
+[ \$(getprop ro.system.build.version.sdk) -lt 33 ] && echo "Android 13 is required! Aborting ..." && abort
+echo ""
+echo "Everything looks fine .... proceeding"
+ui_print ""
+ui_print "Installing Driver Please Wait ..."
+ui_print ""
+
+sleep 1.25
+set_perm_recursive \$MODPATH/system 0 0 755 u:object_r:system_file:s0
+set_perm_recursive \$MODPATH/system/vendor 0 2000 755 u:object_r:vendor_file:s0
+set_perm \$MODPATH/system/vendor/lib64/hw/vulkan.adreno.so 0 0 0644 u:object_r:same_process_hal_file:s0
+
+ui_print "Driver installed Sucessfully"
+sleep 1.25
+
+ui_print ""
+ui_print "All done , Please REBOOT device"
+ui_print ""
+ui_print "BY: @VEKT0R_87"
+ui_print ""
+EOF
+
+
+echo "Packing files in to magisk module ..." $'\n'
+zip -r $workdir/turnip.zip * &> /dev/null
+if ! [ -a $workdir/turnip.zip ];
+	then echo -e "$red-Packing failed!$nocolor" && exit 1
+	else echo -e "$green-All done, you can take your module from here;$nocolor" && echo $workdir/turnip.zip
+fi
