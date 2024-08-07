3317f2b727e436ba5ab653e904162d4e98d23de5
Shankar Vallabhan A <shankarmanu@protonmail.com>
2024-08-07 21:59:36 +0530

    Minor cleanup

--- build-turnip.sh
+++ build-turnip.sh
@@ -3,8 +3,10 @@
 # Required packages for building the turnip driver
 deps="meson ninja patchelf unzip curl pip flex bison zip"
 
-# Android NDK version
+# Android NDK and Mesa version
 ndkver="android-ndk-r27"
+mesaver="https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-24.2.0-rc4/mesa-mesa-24.2.0-rc4.zip"
+mesadir="mesa-mesa-24.2.0-rc4"
 
 # Colors for terminal output
 green='\033[0;32m'
@@ -20,6 +22,7 @@ clear
 if [ -d "$workdir" ]; then
     echo "Work directory already exists. Cleaning before proceeding..." $'\n'
     rm -rf "$workdir"
+    sleep 2
 fi
 
 echo "Checking system for required dependencies..."
@@ -38,7 +41,7 @@ done
 # Install missing dependencies automatically
 if [ "$deps_missing" == "1" ]; then
     echo "Missing dependencies, installing them now..." $'\n'
-    sudo apt install -y meson patchelf unzip curl python3-pip flex bison zip python3-mako python-is-python3 &> /dev/null
+    sudo apt install -y meson ninja patchelf unzip curl python3-pip flex bison zip python3-mako python-is-python3 &> /dev/null
 fi
 
 clear
@@ -55,30 +58,30 @@ clear
 echo "Extracting Android NDK..." $'\n'
 unzip "$ndkver"-linux.zip &> /dev/null
 
-# Download Mesa source code
-echo "Downloading Latest Mesa source from the main branch..." $'\n'
-curl https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip --output mesa-main.zip &> /dev/null
+# Download Mesa source
+echo "Downloading Latest Mesa source ..." $'\n'
+curl $mesaver --output "$mesadir".zip &> /dev/null
 
 clear
 
 echo "Extracting Mesa source..." $'\n'
-unzip mesa-main.zip &> /dev/null
-cd mesa-main
+unzip "$mesadir".zip &> /dev/null
+cd $mesadir
 
 clear
 
 # Create Meson cross file for Android
 echo "Creating Meson cross file..." $'\n'
-ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
+ndk_bin="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
 cat <<EOF >"android-aarch64"
 [binaries]
-ar = '$ndk/llvm-ar'
-c = ['ccache', '$ndk/aarch64-linux-android33-clang', '-O2']
-cpp = ['ccache', '$ndk/aarch64-linux-android33-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++', '-O2']
+ar = '$ndk_bin/llvm-ar'
+c = ['ccache', '$ndk_bin/aarch64-linux-android33-clang', '-fno-semantic-interposition', '-O2']
+cpp = ['ccache', '$ndk_bin/aarch64-linux-android33-clang++', '-fno-semantic-interposition', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++', '-O2']
 c_ld = 'lld'
 cpp_ld = 'lld'
-strip = '$ndk/aarch64-linux-android-strip'
-pkgconfig = ['env', 'PKG_CONFIG_LIBDIR=NDKDIR/pkgconfig', '/usr/bin/pkg-config']
+strip = '$ndk_bin/aarch64-linux-android-strip'
+pkg-config = ['env', 'PKG_CONFIG_LIBDIR=NDKDIR/pkg-config', '/usr/bin/pkg-config']
 [host_machine]
 system = 'android'
 cpu_family = 'aarch64'
@@ -88,14 +91,14 @@ EOF
 
 # Generate build files using Meson
 echo "Generating build files..." $'\n'
-meson build-android-aarch64 --cross-file "$workdir"/mesa-main/android-aarch64 -Dbuildtype=release -Doptimization=2 -Dplatforms=android -Dplatform-sdk-version=33 -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Db_lto=true &> "$workdir"/meson_log
+meson build-android-aarch64 --cross-file "$workdir"/"$mesadir"/android-aarch64 -Dbuildtype=release -Db_pie=true -Dplatforms=android -Dplatform-sdk-version=33 -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Db_lto=true &> "$workdir"/meson_log
 
 # Compile build files using Ninja
 echo "Compiling build files..." $'\n'
 ninja -C build-android-aarch64 &> "$workdir"/ninja_log
 
 echo "Using patchelf to match .so name..." $'\n'
-cp "$workdir"/mesa-main/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so "$workdir"
+cp "$workdir"/"$mesadir"/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so "$workdir"
 cd "$workdir"
 
 if ! [ -a libvulkan_freedreno.so ]; then
@@ -148,7 +151,7 @@ EOF
 cat <<EOF >"module.prop"
 id=turnip-mesa
 name=Freedreno Turnip Vulkan Driver
-version=v24.3
+version=v24.2
 versionCode=1
 author=V3KT0R-87
 description=Turnip is an open-source vulkan driver for devices with Adreno 6xx-7xx GPUs.
@@ -183,20 +186,20 @@ set_perm_recursive \$MODPATH/system 0 0 755 u:object_r:system_file:s0
 set_perm_recursive \$MODPATH/system/vendor 0 2000 755 u:object_r:vendor_file:s0
 set_perm \$MODPATH/system/vendor/lib64/hw/vulkan.adreno.so 0 0 0644 u:object_r:same_process_hal_file:s0
 
-ui_print "Driver installed Sucessfully"
+ui_print "Driver installed Successfully"
 sleep 1.25
 
 ui_print ""
-ui_print "All done , Please REBOOT device"
+ui_print "All done, Please REBOOT device"
 ui_print ""
 ui_print "BY: @VEKT0R_87"
 ui_print ""
 EOF
 
-
-echo "Packing files in to Magisk/KSU module ..." $'\n'
+echo "Packing files into Magisk/KSU module ..." $'\n'
 zip -r $workdir/turnip.zip * &> /dev/null
-if ! [ -a $workdir/turnip.zip ];
-	then echo -e "$red-Packing failed!$nocolor" && exit 1
-	else echo -e "$green-All done, you can take your module from here;$nocolor" && echo $workdir/turnip.zip
+if ! [ -a $workdir/turnip.zip ]; then
+    echo -e "$red-Packing failed!$nocolor" && exit 1
+else
+    echo -e "$green-All done, you can take your module from here;$nocolor" && echo $workdir/turnip.zip
 fi
