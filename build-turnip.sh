802002234f9da896ba4cd7e8a629d26afa2c5ad1
Shankar Vallabhan A <shankarmanu@protonmail.com>
2024-08-28 21:05:22 +0530

    Fixed broken update-binary

--- build-turnip.sh
+++ build-turnip.sh
@@ -119,37 +119,26 @@ mkdir -p "$meta"
 
 # Create update-binary
 cat <<EOF >"$meta/update-binary"
-#!/sbin/sh
-
 #################
 # Initialization
 #################
-
 umask 022
-
 # echo before loading util_functions
 ui_print() { echo "$1"; }
-
 require_new_magisk() {
   ui_print "*******************************"
-  ui_print " Please install Magisk v25.2+! "
+  ui_print " Please install Magisk v20.4+! "
   ui_print "*******************************"
   exit 1
 }
-
 #########################
 # Load util_functions.sh
 #########################
-
 OUTFD=$2
 ZIPFILE=$3
-
-mount /data 2>/dev/null
-
 [ -f /data/adb/magisk/util_functions.sh ] || require_new_magisk
 . /data/adb/magisk/util_functions.sh
-[ $MAGISK_VER_CODE -lt 25200 ] && require_new_magisk
-
+[ $MAGISK_VER_CODE -lt 20400 ] && require_new_magisk
 install_module
 exit 0
 EOF
